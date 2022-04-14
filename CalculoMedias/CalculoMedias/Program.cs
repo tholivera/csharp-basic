d7dea6faaaee577c54af03c0955cf8edc45b61ca
tholivera <emaildathaisoliveira.silva@gmail.com>
2022-04-14 15:08:20 -0300

    Adicionando exercício de cálculo de médias

---
+++ CalculoMedias/CalculoMedias/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CalculoMedias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alunos = new string[4];
+            double[] notas = new double[4];
+            double soma = 0;
+            double media;
+            int aprovado = 0, reprovado = 0, recuperação = 0;
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.WriteLine($"Digite o nome do aluno {i + 1}");
+                alunos[i] = Console.ReadLine();
+                for (int j = 0; j < notas.Length; j++)
+                {
+
+                    Console.WriteLine($"\nDigite a nota do {j + 1} bimestre do(a) aluno(a) {alunos[i]} ");
+                    notas[j] = double.Parse(Console.ReadLine());
+                    soma += notas[j];
+                }
+                media = soma / notas.Length;
+                Console.WriteLine($"\nA média do aluno é {media}");
+
+                if (media >= 5)
+                {
+                    Console.WriteLine($"A situação do(a) aluno(a) {alunos[i]} é: APROVADO\n");
+                    aprovado += 1;
+                }
+                else if (media < 5 && media >= 3)
+                {
+                    Console.WriteLine($"A situação do(a) aluno(a) {alunos[i]} é: RECUPERAÇÃO\n");
+                    recuperação += 1;
+                }
+                else
+                {
+                    Console.WriteLine($"A situação do(a) aluno(a) {alunos[i]} é: REPROVADO\n");
+                    reprovado += 1;
+                }
+
+                soma = 0;
+                Console.WriteLine("----------------------------------------------\n");
+            }
+
+            Console.WriteLine($"Quantidade de alunos APROVADOS: {aprovado}\n");
+            Console.WriteLine($"Quantidade de alunos em RECUPERAÇÃO: {recuperação}\n");
+            Console.WriteLine($"Quantidade de alunos REPROVADOS: {reprovado}\n");
+
+        }
+
+    }
+}
